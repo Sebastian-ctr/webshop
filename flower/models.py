dee678aa811e7b0daaa1e63806c9c1be16e847b9
Sebastian-ctr <75362950+Sebastian-ctr@users.noreply.github.com>
2021-06-28 21:22:44 +0200

    finish backend

--- flower/models.py
+++ flower/models.py
@@ -7,22 +7,36 @@ class Flower(models.Model):
     content = models.TextField(blank=True)
     prize = models.SmallIntegerField()
     sales = models.BooleanField()
-    new_prizes = models.SmallIntegerField(blank=True)
-    order_number = models.SmallIntegerField()
+    new_prizes = models.SmallIntegerField(blank=True, null=True)
+    order_number = models.SmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
 
 class Home(models.Model):
     tittle = models.CharField(max_length=20)
     content = models.TextField()
+
+    def __str__(self):
+        return self.tittle
  
 class About(models.Model):
     tittle = models.CharField(max_length=20)
     content = models.TextField()
+
+    def __str__(self):
+        return self.tittle
 
 class Success(models.Model):
     name = models.CharField(max_length=30)
     image = models.FileField(upload_to='media_cdn')
     content = models.TextField()
     order_number = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name
+    
 
 
 class Contact(models.Model):
--- flower/views.py
+++ flower/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from .models import Home, Flower, About, Success, Contact
 
 # Create your views here.
 def home(request):
-    pass
+    queryset = Home.objects.first()
+    context = {
+        'content':queryset,
+        }
+    return render(request, 'home.html', context)
+
+def shop(request):
+    queryset = Flower.objects.all()
+    context = {
+        "object_list": queryset,
+    }
+    return render(request, 'shop.html', context)
+
+def about(request):
+    queryset = About.objects.first()
+    context = {
+        'about':queryset,
+    }
+    return render(request, 'about.html', context)
+
+def success(request):
+    queryset = Success.objects.all()
+    context = {
+        'success':queryset,
+    }
+    return render(request, 'success.html', context)
+
+def contact(request):
+    querystet = Contact.objects.first()
+    context = {
+        'contact': querystet,
+    }
+    return render(request, 'contact.html', context)
